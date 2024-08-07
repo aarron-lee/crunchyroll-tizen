16a25e495163722826db95bea48f617adc4cb87f
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-08-07 13:57:38 -0400

    add linux electron build

---
+++ electron_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf electron/static
+
+mkdir -p electron/static/build
+cp ./index.html electron/static/build
+cp -rf server/ electron/static/build
+cp -rf img/ electron/static/build
+cp -rf js/ electron/static/build
+cp -rf css/ electron/static/build
+
+# use android auth
+# auth: "Basic eHVuaWh2ZWRidDNtYmlzdWhldnQ6MWtJUzVkeVR2akUwX3JxYUEzWWVBaDBiVVhVbXhXMTE=",
+sed -i 's/auth: "Basic [^"]*",/auth: "Basic eHVuaWh2ZWRidDNtYmlzdWhldnQ6MWtJUzVkeVR2akUwX3JxYUEzWWVBaDBiVVhVbXhXMTE=",/' electron/static/build/server/js/core/service.js
