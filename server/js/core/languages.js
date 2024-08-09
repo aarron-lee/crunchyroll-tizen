03fd1857d89e3e64a1bab2bf1ea65a788a6a3c37
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-08-09 12:20:14 -0400

    add default Profiles key to all languages

--- server/js/core/languages.js
+++ server/js/core/languages.js
@@ -14,6 +14,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "खोजें",
       home: "होम",
       list: "मेरी सूची",
@@ -58,6 +59,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "एप्लिकेशन भाषा",
         audio_lang: "ऑडियो भाषा",
         subtitle_lang: "सबटाइटल भाषा",
@@ -86,6 +88,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "بحث",
       home: "الصفحة الرئيسية",
       list: "قائمتي",
@@ -129,6 +132,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "لغة التطبيق",
         audio_lang: "لغة الصوت",
         subtitle_lang: "لغة الترجمة",
@@ -157,6 +161,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Cerca",
       home: "Inici",
       list: "La meva llista",
@@ -200,6 +205,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Idioma de l'aplicació",
         audio_lang: "Idioma de l'àudio",
         subtitle_lang: "Idioma dels subtítols",
@@ -228,6 +234,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Suchen",
       home: "Startseite",
       list: "Meine Liste",
@@ -271,6 +278,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Anwendungssprache",
         audio_lang: "Audiosprache",
         subtitle_lang: "Untertitelsprache",
@@ -343,6 +351,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Application Language",
         audio_lang: "Audio Language",
         subtitle_lang: "Subtitle Language",
@@ -371,6 +380,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Buscar",
       home: "Inicio",
       list: "Mi lista",
@@ -414,6 +424,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Idioma de la aplicación",
         audio_lang: "Idioma del audio",
         subtitle_lang: "Idioma de los subtítulos",
@@ -442,6 +453,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Rechercher",
       home: "Accueil",
       list: "Ma liste",
@@ -485,6 +497,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Langue de l'application",
         audio_lang: "Langue audio",
         subtitle_lang: "Langue des sous-titres",
@@ -513,6 +526,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Cari",
       home: "Beranda",
       list: "Daftar saya",
@@ -556,6 +570,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Bahasa Aplikasi",
         audio_lang: "Bahasa Audio",
         subtitle_lang: "Bahasa Subjudul",
@@ -584,6 +599,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Cerca",
       home: "Home",
       list: "La mia lista",
@@ -627,6 +643,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Lingua dell'applicazione",
         audio_lang: "Lingua dell'audio",
         subtitle_lang: "Lingua dei sottotitoli",
@@ -655,6 +672,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Cari",
       home: "Laman Utama",
       list: "Senarai Saya",
@@ -698,6 +716,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Bahasa Aplikasi",
         audio_lang: "Bahasa Audio",
         subtitle_lang: "Bahasa Sarikata",
@@ -726,6 +745,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Szukaj",
       home: "Strona główna",
       list: "Moja lista",
@@ -769,6 +789,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Język aplikacji",
         audio_lang: "Język dźwięku",
         subtitle_lang: "Język napisów",
@@ -797,6 +818,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Buscar",
       home: "Página inicial",
       list: "Minha lista",
@@ -840,6 +862,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Idioma da aplicação",
         audio_lang: "Idioma do áudio",
         subtitle_lang: "Idioma das legendas",
@@ -868,6 +891,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Поиск",
       home: "Главная",
       list: "Мой список",
@@ -911,6 +935,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Язык приложения",
         audio_lang: "Язык аудио",
         subtitle_lang: "Язык субтитров",
@@ -939,6 +964,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "ค้นหา",
       home: "หน้าหลัก",
       list: "รายการของฉัน",
@@ -982,6 +1008,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "ภาษาแอปพลิเคชั่น",
         audio_lang: "ภาษาเสียง",
         subtitle_lang: "ภาษาบทย่อย",
@@ -1010,6 +1037,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Ara",
       home: "Ana Sayfa",
       list: "Listem",
@@ -1053,6 +1081,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Uygulama Dili",
         audio_lang: "Ses Dili",
         subtitle_lang: "Altyazı Dili",
@@ -1081,6 +1110,7 @@ window.languages = {
       },
     },
     menu: {
+      profiles: "Profiles",
       search: "Tìm kiếm",
       home: "Trang chủ",
       list: "Danh sách của tôi",
@@ -1124,6 +1154,7 @@ window.languages = {
     },
     settings: {
       menu: {
+        profiles: "Profiles",
         application_lang: "Ngôn ngữ ứng dụng",
         audio_lang: "Ngôn ngữ âm thanh",
         subtitle_lang: "Ngôn ngữ phụ đề",
